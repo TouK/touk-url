2dc66fc7660aa88e693d10eab1157c8a405df660
Przemyslaw Kopanski <P.Kopanski@stud.elka.pw.edu.pl>
2015-08-01 21:34:12 +0200

    First usable release
    Still, it doesn't checks if url is given in form, any string will be coded in db

--- Handler/Decode.hs
+++ Handler/Decode.hs
@@ -1,8 +1,20 @@
+{-# LANGUAGE LambdaCase #-}
 module Handler.Decode where
 
 import Import
 
 getDecodeR :: Text -> Handler Html
 getDecodeR text = do
-    realUrl <- return $ text
+    realUrl <- fmap (\case {(Just url) -> url;
+                             Nothing -> "this url is not in db"})
+              (findShortUrl text)
     defaultLayout $(widgetFile "decode")
+
+findShortUrl :: Text -> Handler (Maybe Text)
+findShortUrl short = do
+  -- look for enties with given encoded url
+  dbProbe <- runDB $ selectList [URLEncoded ==. short] []
+  -- if found, return original url, if not return Nothing
+  case dbProbe of
+    [Entity _ (URL original _)] -> return $ Just original
+    _ -> return Nothing
--- Handler/Encode.hs
+++ Handler/Encode.hs
@@ -10,5 +10,28 @@ getEncodeR = defaultLayout $(widgetFile "get-encode")
 postEncodeR :: Handler Html
 postEncodeR = do
   urlReceived <- runInputPost $ ireq textField "url"
-  encodedUrl <- lift $ R.randomStringWithLen 6
+
+  encodedUrl <- getShortUrl urlReceived
   defaultLayout $(widgetFile "post-encode")
+
+getShortUrl :: Text -> Handler Text
+getShortUrl url = do
+  -- look for entries with given full url
+  dbProbe <- runDB $ selectList [URLOriginal ==. url] []
+  -- if found, return the existing short url
+  -- if not, create the new one
+  case dbProbe of
+    [] -> generateNewShort url
+    (Entity _ (URL _ short)):_ -> return short
+    --_ -> error "should never happen"
+
+generateNewShort :: Text -> Handler Text
+generateNewShort url = do
+  -- generate random string
+  candidate <- lift $ R.randomStringWithLen 6
+  dbProbe <- runDB $ selectList [URLEncoded ==. candidate] []
+  -- if it is already taken, generate new
+  -- if not, insert entry and return it
+  case dbProbe of
+    [] -> fmap (const candidate) (runDB $ insert $ URL url candidate)
+    _ -> generateNewShort url
