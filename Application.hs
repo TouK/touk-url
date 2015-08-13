61c48a474d5ed28b50e9ea41f2ef416c0a64cac8
Maciej Bielecki <zyla@prati.pl>
2015-08-13 21:09:00 +0200

    Remove HomeR, put EncodeR at / instead

--- Application.hs
+++ Application.hs
@@ -31,7 +31,6 @@ import System.Log.FastLogger                (defaultBufSize, newStdoutLoggerSet,
 -- Import all relevant handler modules here.
 -- Don't forget to add new modules to your cabal file!
 import Handler.Common
-import Handler.Home
 import Handler.Encode
 import Handler.Decode
 
